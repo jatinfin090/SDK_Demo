38505cca7dd18bfd5bc2bab64f6106f0860cfc5d
Keshav <jatin.finvasia@gmail.com>
2024-02-12 17:29:01 +0530

    kk
    kkk

--- app/src/main/java/com/example/basedemo/MainActivity.kt
+++ app/src/main/java/com/example/basedemo/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.basedemo
 
+import android.os.Bundle
+import android.util.Log
 import com.example.basedemo.base.BaseActivity
 import com.example.basedemo.databinding.ActivityMainBinding
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override val layoutResId: Int = R.layout.activity_main
     // Additional implementation for MainActivity
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Additional implementation for MainActivity
+        //  printValues()
+    }
+
+    public fun printValues() {
+        Log.e("SDK_CALL", "Value from SDK")
+    }
 }
--- app/src/main/java/com/example/basedemo/base/BaseApplication.kt
+++ app/src/main/java/com/example/basedemo/base/BaseApplication.kt
@@ -1,9 +1,8 @@
 package com.example.basedemo.base
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+
 class BaseApplication : Application() {
 
 
